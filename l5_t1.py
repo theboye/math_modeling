6d91bc5a79ede9c2435342a3ecda36dcee07c16e
theboye <91481824+theboye@users.noreply.github.com>
2021-11-15 16:39:32 +0000

    i did

---
+++ l5_t1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+l1st = np.random.randint(-69, 69, 69)
+b = np.array(l1st)
+
+def lol():
+  return np.mean(b)
---
+++ l5_t4.py
@@ -0,0 +1,4 @@
+a,b,N = float(input()),float(input()),int(input())
+def function(y):
+  coords = np.zeros((N))
+for t in np.linspace(a,b,N):
