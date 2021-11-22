4e01cda13ae8d30e9ef123f8a632ae3dd5ad2189
theboye <91481824+theboye@users.noreply.github.com>
2021-11-22 15:50:47 +0000

    extra work

--- less 2/l2_t4.py
+++ less 2/l2_t4.py
@@ -1,14 +1,2 @@
-a = int(input())
-b = 1
-c = 2
-if a == 1:
-  print(1)
-elif a == 2:
-  print(b , b)
-else:
-  print(b , b , c ,end=' ')
-  for i in range(3 , a+1):
-    print(b+c , end=' ')
-    e = b
-    b = c
-    c = e + b
+n = int(input())
+a,b = 1,1
